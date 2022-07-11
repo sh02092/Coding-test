0126c7186a7aa6b6b72d75e14528c7901e063ad7
Sanghyun <sh02092@naver.com>
2022-07-11 17:58:41 +0900

    220712 inha algorithm study

--- Python_Projects/5월2주차/11721_열개씩끊어출력하기.py
+++
@@ -1,4 +0,0 @@
-word=input()
-for i in range(0, len(word), 10):
-    temp = i + 10
-    print(word[i : temp])
--- Python_Projects/week2/test.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-while 1:
-    print(np.random.randint((1, 10, 1)))
