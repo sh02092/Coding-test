291289a165b07f8271ff9e7493a956db40a7a3e6
Sanghyun <sh02092@naver.com>
2022-08-17 23:08:15 +0900

    220817-commit

--- Python_Projects/8월 2주차/220810/BJ_17779.py
+++ Python_Projects/8월 2주차/220810/BJ_17779.py
@@ -1,3 +1,17 @@
 # https://www.acmicpc.net/problem/17779
 # 게리맨더링2- BJ_17779
 
+import sys  
+from collections import deque  
+N, K = map(int, sys.stdin.readline().rstrip().split())  
+order_queue = deque()  
+ret_str = "<"  
+for i in range(N):  
+    order_queue.append(i+1)  
+while order_queue:  
+    for i in range(K-1):  
+        tmp_elem = order_queue.popleft()  
+        order_queue.append(tmp_elem)  
+    ret_str += "{}, ".format(order_queue.popleft())  
+ret_str = ret_str[:-2] + ">"  
+print(ret_str)
