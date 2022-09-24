96fa9e5f484fe3c5a3be756fe128df56ac9f8ebd
Sanghyun <sh02092@naver.com>
2022-09-24 20:20:39 +0900

    220924-commit

--- Python_Projects/9월 4주차/220923/BJ_8979.py
+++ Python_Projects/9월 4주차/220923/BJ_8979.py
@@ -6,5 +6,9 @@ country = [[] for _ in range(k + 1)]
 
 for i in range(n):
     c, gold, silver, bronze = map(int, input().split())
-    country[c] = [gold, silver, bronze]
-    
+    country[c] = [gold, silver, bronze, c]
+country.sort(key=lambda x:x[0])
+country.sort(key=lambda x:x[1])
+country.sort(key=lambda x:x[2])
+
+print(country.index())
