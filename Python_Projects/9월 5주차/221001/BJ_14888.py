341ca83848e84bd950d4b5c32dd196da5f41a00e
Sanghyun <sh02092@naver.com>
2022-10-26 00:00:51 +0900

    commit-221026

--- Python_Projects/9월 5주차/221001/BJ_14888.py
+++ Python_Projects/9월 5주차/221001/BJ_14888.py
@@ -1,25 +1,26 @@
-# https://www.acmicpc.net/problem/14888
-# 연산자 끼워넣기- BJ_14888
-s = '3people    UnFo3lowed me'
-s = s.capitalize()
-print(s)
-# 홍보자료로 발탁
-# ITRC 잘 풀어서 말하기
-
 def solution(s):
-    answer = ''
-    temp = s.split(' ')
-    for i in range(len(temp)):
-        if temp[i]:
-            if 97 <= ord(temp[i][0]) <= 122:
-                temp[i] = temp[i][0].upper() + temp[i][1:].lower()
-            else:
-                temp[i] = temp[i][0] + temp[i][1:].lower()
-    # print(temp)
-    answer = ' '.join(temp)
-    # 65 ~ 90, 97 ~ 122
-    
-    
-    return answer
+    answer = -1
+    len_s = 0
+    s = list(s)
+    while 1:
+        len_s = len(s)
+        if len(s) == 0:
+            break
+        else:
+            i = 1
+            while 1:
+                if i > len(s):
+                    break
+                if s[i - 1] == s[i]:
+                    del s[i - 1:i + 1]
+                i += 1
+                
+        if len_s == len(s):
+            break
+            
+    if len_s == 0:
+        return 1
+    else:
+        return 0
 
-print(solution(s))
+print(solution('cdcd'))
