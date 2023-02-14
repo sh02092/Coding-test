42fe773fe1658df6bffcf88ed63d040ce5870c60
Sanghyun Kim <86821581+sh02092@users.noreply.github.com>
2023-02-14 21:53:27 +0900

    230214-commit

--- 2023 Python_Projects/2월3주차/BJ_21608_new.py
+++ 2023 Python_Projects/2월3주차/BJ_21608_new.py
@@ -1,70 +1,3 @@
-# Solving
-
-## NBA 농구
-https://www.acmicpc.net/problem/2852
-### 문제풀이
-```python
-import sys
-input = sys.stdin.readline
-
-n = int(input())
-win = [0, 0]
-winT = [0, 0]
-startT = 0
-endT = 48 * 60
-temp = -1
-
-for i in range(n):
-    teamNum, goalTime = map(str, input().split())
-    num = int(teamNum) - 1
-    m, s = goalTime.split(':')
-    time = int(m) * 60 + int(s)
-    win[num] += 1
-    
-    if win[0] > win[1] and win[0] == 1:     # 1번팀 이길때
-        startT = time
-        temp = 0
-    elif win[0] < win[1] and win[1] == 1:   # 2번팀 이길때
-        startT = time
-        temp = 1
-    elif win[0] == win[1]:  # 비길때
-        endT = time
-        winT[temp] += endT - startT
-
-        # 초기화
-        endT = 48 * 60
-        win[0] = 0
-        win[1] = 0
-
-if endT == 48 * 60 and win[0] != win[1]:    # 어느 팀이든 이기고 있을때
-    winT[temp] += endT - startT
-
-ans1M = str(winT[0] // 60)
-ans1S = str(winT[0] % 60)
-ans2M = str(winT[1] // 60)
-ans2S = str(winT[1] % 60)
-
-if int(ans1M) < 10:
-    ans1M = '0' + ans1M
-if int(ans1S) < 10:
-    ans1S = '0' + ans1S
-if int(ans2M) < 10:
-    ans2M = '0' + ans2M
-if int(ans2S) < 10:
-    ans2S = '0' + ans2S
-
-print(ans1M + ':' + ans1S)
-print(ans2M + ':' + ans2S)
-```
-1번팀이 이기고 있을때와 2번팀이 이기고 있을때의 경우를 잘 나눠 생각해서 푸는 구현문제
-### 의견
-반례를 생각하는게 제법 까다로웠던 구현문제였다. 좋은 문제!
-
-
-## 상어 초등학교
-https://www.acmicpc.net/problem/21608
-### 문제풀이
-```python
 # https://www.acmicpc.net/problem/21608
 # 상어 초등학교- BJ_21608
 
@@ -179,7 +112,3 @@ for y in range(n):
             ans += 1000
 
 print(ans)
-```
-빡센 구현문제, 1, 2, 3번 조건을 차례차례 따져가며 자리 배치를 하면 된다. 3번 조건의 경우 for문을 돌면서 자동으로 정해지기에 따로 지정하지 않아도 된다. 
-### 의견
-예제 및 반례 케이스까지 모두 통과하여 풀었다고 생각했지만.. 틀렸다. 왜 틀렸을까.. 아기 상어 문제처럼... 혼란스럽다. 4시간은 투자한 것 같은데 뭐가 부족했던걸까
